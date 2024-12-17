f75cfcb46848e84666748ca73f1e87a6f4436483
shery <sherypagal123@gmail.com>
2024-12-17 14:23:58 +0500

    project added

---
+++ app.js
@@ -0,0 +1,126 @@
+
+// CHAPTER 1:
+
+// Question no 1:
+
+// alert("hello world");
+
+// Question no 2:
+
+// alert("Error! please enter your valid password");
+
+// Question no 3:
+
+// alert("welcome to js land.. \n happy coding!");
+
+// Question no 4:
+
+// alert("what about your new password");
+
+// alert("prevents this page from creating additional dialogs.");
+
+// Question no 5:
+
+// console.log("hello.. i can run js my web browser console");
+
+// Questino no 6:
+
+// 
+
+// Question no 7:
+
+// 
+
+// CHAPTER no 2:
+
+// Question no 1:
+
+// var username ;
+
+// Question no 2:
+
+// var myname ="Your Full Name"
+
+// Question no 3:
+
+// var message ;
+
+// message = "“Hello World”";
+
+// alert(message);
+
+// Question no 4:
+
+// var studentName ="Jone Doe"
+
+// alert(studentName);
+
+// var studentAge = "15 years old";
+
+// alert(studentAge);
+
+// var studentStudies = "Certified Mobile Appliction Development"
+
+// alert(studentStudies);
+
+// Question no 5:
+
+// var piza = "PIZZA\nPIZZ\nPIZ\nPI\nP"
+
+// alert (piza);
+
+// Question no 6:
+
+// var email = "example@example.com."
+
+// alert (email);
+
+// Question no 7
+
+// var book = "A smarter way to learn JavaScript."
+
+// alert (book);
+
+// Question no 8
+
+// var display ="Yah! I can write HTML content through JavaScript"
+
+// document.write(display);
+
+// Question no 9
+
+// var symbol ="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+
+// alert(symbol);
+
+// document.write(symbol);
+
+// chapter 3:
+
+// Question no 1:
+
+// var age =" am 15 years old"
+
+//  alert(age);
+
+// Question no 2:
+
+// var visitor="you have visited this site 14 times"
+
+//  alert(visitor);
+
+// Question no 3:
+
+// var barthYear ="My dirth year is 1990 <br> Data type of my ddeclared variable is number"
+
+//  document.write(barthYear);
+
+// Question no 4:
+
+// var VisitorsName = " <b> JSON Doe </b>"
+
+// var ProductTitle =" <b> T-shirt</b>"
+
+// var quantity = 5 
+
+// document.write(VisitorsName+"orders "+"<b>"+quantity +"</b>"+ProductTitle+"(s) on XYZ Clothing store");
